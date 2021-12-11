fe1fd9ac82268ce9243c1d47df36b5bedaf228a8
jazib-sudo <jazib.prof@gmail.com>
2021-12-11 23:13:26 +0530

    Upper bound update

---
+++ W6D1/up_bd_hard.py
@@ -0,0 +1,24 @@
+def lower(arr,target):
+    low = 0
+    high = len(arr)-1
+    if target < arr[0]:
+        return 0
+    if target==arr[high]:
+        return high
+    while(low<=high):
+        mid = (low+high)//2
+        if arr[mid]==target and target<arr[mid+1]:#arr[mid+1]>target
+            return mid
+        elif arr[mid]==target and arr[mid+1]==target:
+            low = mid+1
+        elif arr[mid-1]<target and arr[mid+1]>target:
+            return mid
+        elif arr[mid]<target:
+            low = mid+1
+        elif arr[mid]>target:
+            high = mid-1
+arr = [1,1,1,3,3,4,5,5,5]
+#      0 1 2 3 4 5 6 7 
+target = 2
+print(lower(arr,target))
+
